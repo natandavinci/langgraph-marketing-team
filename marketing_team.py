14a5c1a0c698a1e49d7f9cfa86a50f18eee32530
natandavinci <natanaelqueiroz969@gmail.com>
2026-06-23 14:34:07 -0300

    primeiro nó feito

--- marketing_team.py
+++ marketing_team.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
-class Graphstate(TypedDict):
+class GraphState(TypedDict):
     tema: str
     pesquisa: Optional[str]
     post_final: Optional[str]
@@ -24,3 +24,29 @@ class SupervisorDecision(BaseModel):
     )
 
     justificativa: str = Field(description="Breve razão de ter escolhido essa ação.")
+
+# NODE 1
+def researcher_node(state: GraphState) -> dict:
+    print("\n🔍 [AGENTE PESQUISADOR]: Buscando dados e tendências sobre o tema...")
+   
+    tema = state["tema"]
+
+    prompt = f"""
+    Você é um Analista de Business Intelligence (BI) e Pesquisador de Mercado Sênior.
+    Sua tarefa é coletar insights, dados relevantes, dores do público-alvo e gatilhos mentais sobre o tema: "{tema}".
+    
+    Traga informações brutas e insights profundos estruturados em tópicos:
+    1. Estatísticas ou fatos relevantes (pode estimar baseado no conhecimento do modelo).
+    2. Principais dores ou desejos do público sobre isso.
+    3. Três palavras-chave ou conceitos que não podem ficar de fora.
+    
+    Seja puramente analítico e informativo. Não escreva o post final.
+    """
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return {"pesquisa": response.text}
+
